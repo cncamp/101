3eb9b9177ccbaadc71779f04da5f7ef50117b611
fmeng <fmeng@ebay.com>
2020-06-13 09:37:48 +0800

    update instruction

--- minikube_start.sh
+++ minikube_start.sh
@@ -1,10 +1,10 @@
 minikube start \
-    --cpus 4 \
-    --memory 4096 \
+    --cpus=8 \
+    --v=4 \
+    --memory=8192 \
     --network-plugin=cni \
     --enable-default-cni \
     --bootstrapper=kubeadm \
-    --kubernetes-version v1.15.4 \
+    --kubernetes-version v1.18.3 \
     --image-mirror-country=cn \
     --image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers
-    
