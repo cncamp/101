4be5d0ac60c5bc7b39dcb06381bc2b8b2d8c0a2e
fmeng <fmeng@ebay.com>
2021-09-08 23:08:43 +0800

    add tekton and kind

---
+++ kind/cluster-api/generate_workload_cluster.sh
@@ -0,0 +1,6 @@
+clusterctl init --infrastructure docker --config dockercluster.yaml
+clusterctl generate cluster capi-quickstart --flavor development \
+  --kubernetes-version v1.22.0 \
+  --control-plane-machine-count=1 \
+  --worker-machine-count=1 \
+  > capi-quickstart.yaml
---
+++ kind/create_cluster.sh
@@ -0,0 +1,2 @@
+env KIND_EXPERIMENTAL_DOCKER_NETWORK=bridge 
+kind create cluster --config ./kind.conf
