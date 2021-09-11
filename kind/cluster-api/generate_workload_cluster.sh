dbd24f72a959add4dc3656d58b6092b4c0a7a037
fmeng <fmeng@ebay.com>
2021-09-11 16:54:06 +0800

    add loki

--- kind/cluster-api/generate_workload_cluster.sh
+++ kind/cluster-api/generate_workload_cluster.sh
@@ -1,4 +1,3 @@
-clusterctl init --infrastructure docker --config dockercluster.yaml
 clusterctl generate cluster capi-quickstart --flavor development \
   --kubernetes-version v1.22.0 \
   --control-plane-machine-count=1 \
