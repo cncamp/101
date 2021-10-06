0226a6c80dc59de4da6f6e8f95f5d79d494ef515
mfanjie <mengfj@gmail.com>
2021-10-06 15:07:06 +0800

    add webhook

---
+++ go.mod
@@ -0,0 +1,30 @@
+module github.com/cncamp/101
+
+go 1.17
+
+require (
+	github.com/google/go-github v17.0.0+incompatible
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1
+	k8s.io/api v0.22.2
+)
+
+require (
+	github.com/go-logr/logr v0.4.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20210520170846-37e1c6afe023 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.22.2 // indirect
+	k8s.io/klog/v2 v2.9.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.1.2 // indirect
+)
---
+++ module6/authn-webhook/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	authentication "k8s.io/api/authentication/v1"
+)
+
+func main() {
+	http.HandleFunc("/authenticate", func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		var tr authentication.TokenReview
+		err := decoder.Decode(&tr)
+		if err != nil {
+			log.Println("[Error]", err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"apiVersion": "authentication.k8s.io/v1beta1",
+				"kind":       "TokenReview",
+				"status": authentication.TokenReviewStatus{
+					Authenticated: false,
+				},
+			})
+			return
+		}
+		log.Printf("receving request", tr)
+		// Check User
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: tr.Spec.Token},
+		)
+		tc := oauth2.NewClient(oauth2.NoContext, ts)
+		client := github.NewClient(tc)
+		user, _, err := client.Users.Get(context.Background(), "")
+		if err != nil {
+			log.Println("[Error]", err.Error())
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"apiVersion": "authentication.k8s.io/v1beta1",
+				"kind":       "TokenReview",
+				"status": authentication.TokenReviewStatus{
+					Authenticated: false,
+				},
+			})
+			return
+		}
+		log.Printf("[Success] login as %s", *user.Login)
+		w.WriteHeader(http.StatusOK)
+		trs := authentication.TokenReviewStatus{
+			Authenticated: true,
+			User: authentication.UserInfo{
+				Username: *user.Login,
+				UID:      *user.Login,
+			},
+		}
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"apiVersion": "authentication.k8s.io/v1beta1",
+			"kind":       "TokenReview",
+			"status":     trs,
+		})
+	})
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
